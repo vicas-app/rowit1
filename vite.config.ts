93c61d7cd02835b71b0bbb9661012186035c69e3
vicas <vicas.emporium1@gmail.com>
2025-10-07 08:56:57 +0100

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
       plugins: [react()],
+      base: "/rowit1/", // <-- change this!
+
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
